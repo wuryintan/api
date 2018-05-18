f99121b52f69f3c9c3cf7a697178440c5ae21f62
SalatID <root.mursalat@gmail.com>
2018-05-18 15:24:31 +0700

    downgrade jwt-auth
    Downgrade from 1.0.0-rc.1 to 0.5.12

--- api/app/User.php
+++ api/app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
+//use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable ////implements JWTSubject
 {
 	protected $table = 'user';
 	public $timestamps = false;
@@ -29,18 +29,18 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password', 'remember_token',
     ];
-	public function getJWTIdentifier()
+	/*public function getJWTIdentifier()
     {
         return $this->getKey();
-    }
+    }*/
 
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
      * @return array
      */
-    public function getJWTCustomClaims()
+    /*public function getJWTCustomClaims()
     {
         return [];
-    }
+    }*/
 }
